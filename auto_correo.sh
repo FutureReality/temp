96ce5db5a79f1e50ee11870587468461886dca85
FutureReality <115637305+FutureReality@users.noreply.github.com>
2025-05-16 15:57:07 +0200

    auto_correo.sh

--- auto_correo.sh
+++ auto_correo.sh
@@ -5,7 +5,6 @@ DB_PASS="auditor"
 DB_NAME="CI_NA_Tickets"
 SMTP_FROM="no-reply@computerinnovations.com"
 
-
 mysql -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" -se \
 "SELECT JSON_OBJECT('id', id, 'email', email, 'subject', subject, 'message', message) FROM pending_mails WHERE sent = 0;" | \
 while read -r json; do
@@ -15,13 +14,14 @@ while read -r json; do
     message=$(echo "$json" | jq -r .message)
 
     mail_subject="Ticket $id"
-    mail_body="Su ticket con id $id ha sido llegado a los tecnicos.
+    mail_body="Su ticket con id $id ha sido llegado a los técnicos.
 
 Título: $subject
 
 Mensaje: $message"
 
-    echo "$mail_body" | mail -s "$mail_subject" -r "$SMTP_FROM" "$email"
+    # Enviar correo con remitente "Helpdesk"
+    echo -e "From: Helpdesk <$SMTP_FROM>\nTo: $email\nSubject: $mail_subject\n\n$mail_body" | /usr/sbin/sendmail -t
 
     if [ $? -eq 0 ]; then
         mysql -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" -e "DELETE FROM pending_mails WHERE id = $id"
